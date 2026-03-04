1b2dea6f0a821f0034dc4dee4fe64d24c979c711
iliailia12 <iliakviciani392@gmail.com>
2026-03-04 18:57:20 +0400

    second commit

---
+++ .history/Day 096/Classwork/task2_20260227164919.js
@@ -0,0 +1,12 @@
+// ### დავალება 2
+js
+const user = {
+  name: "Gio",
+  age: 25,
+  city: "Tbilisi",
+  salary: 1200
+}
+
+for...in გამოყენებით დაბეჭდე მხოლოდ ის properties, რომელთა მნიშვნელობა არის რიცხვი.
+
+---
---
+++ .history/Day 096/Classwork/task2_20260227164934.js
@@ -0,0 +1,9 @@
+// ### დავალება 2
+const user = {
+    name: "Gio",
+    age: 25,
+    city: "Tbilisi",
+    salary: 1200
+}
+
+for...in გამოყენებით დაბეჭდე მხოლოდ ის properties, რომელთა მნიშვნელობა არის რიცხვი.
