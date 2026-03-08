7c35c5001ab142bac9252833bf76a7ab41e7782f
iliailia12 <iliakviciani392@gmail.com>
2026-03-09 00:15:50 +0400

    second commit

---
+++ .history/Day 098/Homework/task2_20260308194941.js
@@ -0,0 +1,17 @@
+// 2) შექმენი factory function რომელიც შექმნის ობიექტებს კუთვნილებებით: hobby1, hobby2, hobby3 და მეთოდით myHobbies() რომელიც გამოიტანს ერთ დიდ წინადადებას შენი ჰობიების შესახებ
+
+function createHobbies(hobby1, hobby2, hobby3) {
+  return {
+    hobby1: hobby1,
+    hobby2: hobby2,
+    hobby3: hobby3,
+
+    myHobbies: function () {
+      console.log("My hobbies are " + this.hobby1 + ", " + this.hobby2 + " and " + this.hobby3 + ".") 
+    }
+  } 
+}
+
+
+const myHobbyObject = createHobbies("reading", "gaming", "football") 
+myHobbyObject.myHobbies()
