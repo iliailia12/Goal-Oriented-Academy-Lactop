7f78527dd3d63c197ccf05da3e78f02821934cb0
iliailia12 <iliakviciani392@gmail.com>
2026-03-08 19:43:42 +0400

    second commit

---
+++ .history/Day 098/Classwork/task1_20260308192700.js
@@ -0,0 +1,16 @@
+// <!-- 1)შექმენი ორი ობიექტი, ორივე ობიექტში შეინახე მანქანის ბრენდი და მოდელი, ორივე ობიექტში დაამატე ერთ და იგივე ფუნქცია სახელად aboutCar(), რომელიც გამოძახებისას გამოიტანს წინადადებას "this is (brand)(model)" იმ მანქანაზე რომლის ობიექტშიც გამოვიძახეთ -->
+
+const car1 = {
+    brand: 'BMW',
+    model: 'M5',
+
+    abautCar: function(){
+        console.log('this is'+this.brand+''+this.model)
+    }
+}
+
+const car2 = {
+    brand: 'Mersedes',
+    model: 'C63'
+    
+}
---
+++ .history/Day 098/Homework/task1_20260308194240.js
@@ -0,0 +1,18 @@
+// 1)შექმენი ორი ობიექტი, ორივე ობიექტში შეინახე ტელეფონის ბრენდი და მოდელი, ორივე ობიექტში დაამატე ერთ და იგივე ფუნქცია სახელად aboutTelephone(), რომელიც გამოძახებისას გამოიტანს წინადადებას "this is (brand)(model)" იმ ტელეფონზე რომლის ობიექტშიც გამოვიძახეთ
+
+let phone1 = {
+    brand: 'Apple',
+    model: 'Iphone 18',
+    aboutTelephone: function() {
+        console.log("this is " + this.brand + " " + this.model)
+    }
+}
+
+let phone2 = {
+  brand: "Samsung",
+  model: "Galaxy S23",
+  
+  aboutTelephone: function() {
+    console.log("this is " + this.brand + " " + this.model)
+  }
+}
